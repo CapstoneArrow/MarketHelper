c976a44d8eac28ba01355836bda86ac22816d961
jhyeonc <jhyeonc01@gmail.com>
2024-05-03 00:47:03 +0900

    cleaned up

--- Markets/Views/MorePage/Nearby/NearbyView.swift
+++ Markets/Views/MorePage/Nearby/NearbyView.swift
@@ -27,17 +27,19 @@ struct NearbyView: View {
             .onAppear {
                 CLLocationManager().requestWhenInUseAuthorization()
             }
-            
         }
     }
 }
 
+// 지도의 기본 위치와 배율을 설정
+// 기본 위치 - 한림대학교 공학관
 extension CLLocationCoordinate2D {
     static var defaultUserLocation: CLLocationCoordinate2D {
         return .init(latitude: 37.886329, longitude: 127.735745)
     }
 }
 
+// 기본 배율 설정
 extension MKCoordinateRegion {
     static var userRegion: MKCoordinateRegion {
         return .init(center: .defaultUserLocation,
@@ -49,4 +51,3 @@ extension MKCoordinateRegion {
 #Preview {
     NearbyView()
 }
-
